5508f078acd2c9f783f04129ee21ad87c56429f8
awesomething <79168736+PairApp@users.noreply.github.com>
2024-05-01 22:58:25 -0400

    update

--- controllers/bands_controller.js
+++ controllers/bands_controller.js
@@ -1,29 +1,78 @@
 // DEPENDENCIES
 const bands = require('express').Router()
 const db = require('../models')
-const { Band } = db 
+// DEPENDENCIES 
+const { Band, MeetGreet, Event, SetTime } = db 
+const { Op } = require('sequelize')
 
 // FIND ALL BANDS
 bands.get('/', async (req, res) => {
     try {
-        const foundBands = await Band.findAll()
+        const foundBands = await Band.findAll({
+            order: [ [ 'available_start_time', 'ASC' ] ],
+            where: {
+                name: { [Op.like]: `%${req.query.name ? req.query.name : ''}%` }
+            }
+        })
         res.status(200).json(foundBands)
     } catch (error) {
         res.status(500).json(error)
     }
 })
+// bands.get('/', async (req, res) => {
+//     try {
+//         const foundBands = await Band.findAll()
+//         res.status(200).json(foundBands)
+//     } catch (error) {
+//         res.status(500).json(error)
+//     }
+// })
 
 // FIND A SPECIFIC BAND
-bands.get('/:id', async (req, res) => {
+// bands.get('/:id', async (req, res) => {
+//     try {
+//         const foundBand = await Band.findOne({
+//             where: { band_id: req.params.id }
+//         })
+//         res.status(200).json(foundBand)
+//     } catch (error) {
+//         res.status(500).json(error)
+//     }
+// })
+
+
+// FIND A SPECIFIC BAND
+bands.get('/:name', async (req, res) => {
     try {
         const foundBand = await Band.findOne({
-            where: { band_id: req.params.id }
+            where: { name: req.params.name  },
+    include: [ 
+        { 
+            model: MeetGreet, 
+            as: "meet_greets",
+            include: { 
+                model: Event, 
+                as: "event",
+                where: { name: { [Op.like]: `%${req.query.event ? req.query.event : ''}%` } }
+            } 
+        },
+        { 
+            model: SetTime,
+            as: "set_times",
+            include: { 
+                model: Event, 
+                as: "event",
+                where: { name: { [Op.like]: `%${req.query.event ? req.query.event : ''}%` } }
+            }
+        }
+    ] 
         })
         res.status(200).json(foundBand)
     } catch (error) {
         res.status(500).json(error)
     }
 })
+
 
 // CREATE A BAND
 bands.post('/', async (req, res) => {
